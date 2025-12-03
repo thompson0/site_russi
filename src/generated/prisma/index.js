f88a603ce3b38f3b95cdd2662e454d8fe83a0825
thompson0 <thompsonjooj@gmail.com>
2025-12-02 21:19:42 -0300

    fix: prisma bug

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -256,8 +256,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../../../prisma",
   "clientVersion": "6.17.1",
